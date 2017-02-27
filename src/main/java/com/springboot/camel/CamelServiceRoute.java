1892f28b2f0a5749a8869903e9e5d1fed599d687
Prigya <prigya.shukla@gmail.com>
2017-02-27 14:44:47 +0530

    cxf proxy endpoint changes

--- src/main/java/com/springboot/camel/CamelServiceRoute.java
+++ src/main/java/com/springboot/camel/CamelServiceRoute.java
@@ -12,7 +12,7 @@ public class CamelServiceRoute extends RouteBuilder{
 		
 		from("timer:foo?fixedRate=true&period=5000&delay=3000")
 		//.to("restlet:http://localhost:8081/employees?restletMethod=get")
-		.to("restlet:http://employeedetails01-lean-architecture-poc.44fs.preview.openshiftapps.com/employees?restletMethod=get")
+		.to("restlet:http://emp-detail-master-camelintegrationmicroservicedemo.44fs.preview.openshiftapps.com/employees?restletMethod=get")
 			.process(new Processor(){
 				@Override
 		         public void process(Exchange exchange) throws Exception {
@@ -26,7 +26,7 @@ public class CamelServiceRoute extends RouteBuilder{
 			         exchange.getOut().setBody(req);
 				}
 		       })
-		.to("restlet:http://emptimerecords01-lean-architecture-poc.44fs.preview.openshiftapps.com?restletMethod=post");
+		.to("restlet: http://emp-time-record-camelintegrationmicroservicedemo.44fs.preview.openshiftapps.com?restletMethod=post");
 		
 		
 		
